49bbf5c7bf9889af8e20d1e17d886a5d43a589c7
David Seibel <dseibel88@gmail.com>
2018-10-19 08:03:29 -0400

    Improved validation of agents and agent groups.

--- Functions/Public/Start-AMTask.ps1
+++ Functions/Public/Start-AMTask.ps1
@@ -10,10 +10,10 @@ function Start-AMTask {
             The tasks to start.
 
         .PARAMETER Agent
-            The agent to run the process on.
+            The agent to run the task on.
 
         .PARAMETER AgentGroup
-            The agent group to run the process on.
+            The agent group to run the task on.
 
         .INPUTS
             Tasks can be supplied on the pipeline to this function.
@@ -26,7 +26,7 @@ function Start-AMTask {
             Author(s):     : David Seibel
             Contributor(s) :
             Date Created   : 07/26/2018
-            Date Modified  : 10/16/2018
+            Date Modified  : 10/19/2018
 
         .LINK
             https://github.com/davidseibel/AutoMatePS
@@ -52,33 +52,54 @@ function Start-AMTask {
             if ($obj.Type -eq "Task") {
                 switch($PSCmdlet.ParameterSetName) {
                     "Agent" {
-                        if ($Agent.Type -ne "Agent") {
+                        if ($Agent -is [string]) {
                             $name = $Agent
-                            $Agent = Get-AMAgent -Name $name -Connection $obj.ConnectionAlias
-                            if (($Agent | Measure-Object).Count -ne 1) {
+                            # Can't assign agent directly because of ValidateNotNullOrEmpty on parameter
+                            $tempAgent = Get-AMAgent -Name $name -Connection $obj.ConnectionAlias
+                            if (($tempAgent | Measure-Object).Count -eq 1) {
+                                $Agent = $tempAgent
+                            } else {
                                 throw "Agent '$name' not found!"
                             }
                         }
-                        if ($Agent.AgentType -eq "TaskAgent") {
-                            Write-Verbose "Running task $($obj.Name) on agent $($Agent.Name)."
-                            $runUri = Format-AMUri -Path "tasks/$($obj.ID)/run" -Parameters "agent_id=$($Agent.ID)"
+                        if ($Agent.Type -eq "Agent") {
+                            if ($Agent.AgentType -eq "TaskAgent") {
+                                if ($Agent.ConnectionAlias -eq $obj.ConnectionAlias) {
+                                    Write-Verbose "Running task $($obj.Name) on agent $($Agent.Name)."
+                                    $runUri = Format-AMUri -Path "tasks/$($obj.ID)/run" -Parameters "agent_id=$($Agent.ID)"
+                                } else {
+                                    throw "Task '$($obj.Name)' and agent '$($Agent.Name)' are not on the same server!" 
+                                }
+                            } else {
+                                throw "Agent $($Agent.Name) is not a task agent!"
+                            }
                         } else {
-                            throw "Agent $($Agent.Name) is not a process agent!"
+                            throw "Unsupported agent type '$($Agent.Type)' encountered!"
                         }
                     }
                     "AgentGroup" {
-                        if ($AgentGroup.Type -ne "AgentGroup") {
+                        if ($AgentGroup -is [string]) {
                             $name = $AgentGroup
-                            $AgentGroup = Get-AMAgentGroup -Name $name -Connection $obj.ConnectionAlias
-                            if (($AgentGroup | Measure-Object).Count -ne 1) {
+                            # Can't assign agent directly because of ValidateNotNullOrEmpty on parameter
+                            $tempAgentGroup = Get-AMAgentGroup -Name $name -Connection $obj.ConnectionAlias
+                            if (($tempAgentGroup | Measure-Object).Count -eq 1) {
+                                $AgentGroup = $tempAgentGroup
+                            } else {
                                 throw "Agent group '$name' not found!"
                             }
                         }
-
-                        Write-Verbose "Running task $($obj.Name) on agent group $($AgentGroup.Name)."
-                        $runUri = Format-AMUri -Path "tasks/$($obj.ID)/run" -Parameters "agent_group_id=$($AgentGroup.ID)"
+                        if ($AgentGroup.Type -eq "AgentGroup") {
+                            if ($AgentGroup.ConnectionAlias -eq $obj.ConnectionAlias) {
+                                Write-Verbose "Running task $($obj.Name) on agent group $($AgentGroup.Name)."
+                                $runUri = Format-AMUri -Path "tasks/$($obj.ID)/run" -Parameters "agent_group_id=$($AgentGroup.ID)"
+                            } else {
+                                throw "Task '$($obj.Name)' and agent group '$($AgentGroup.Name)' are not on the same server!" 
+                            }
+                        } else {
+                            throw "Unsupported agent group type '$($AgentGroup.Type)' encountered!"
+                        }
                     }
-                }                
+                }
                 $instanceID = Invoke-AMRestMethod -Resource $runUri -RestMethod Post -Connection $obj.ConnectionAlias
                 Start-Sleep -Seconds 1   # The instance can't be retrieved right away, have to pause briefly
                 $listUri = Format-AMUri -Path "instances/list" -FilterSet @{Property = "ID"; Operator = "="; Value = $instanceID}
