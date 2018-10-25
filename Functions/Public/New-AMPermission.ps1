4686f96dd3f2eb753bc85da3e9a78bdb88eb34d5
David Seibel <dseibel88@gmail.com>
2018-10-25 11:03:48 -0400

    Add parameter descriptions and remove unused CanEdit parameter for New-AMPermission.

--- Functions/Public/New-AMPermission.ps1
+++ Functions/Public/New-AMPermission.ps1
@@ -12,6 +12,75 @@ function New-AMPermission {
         .PARAMETER Principal
             The user or group to assign security to.
 
+		.PARAMETER FullControl
+			Allow or deny full control on the object.
+
+		.PARAMETER Create
+			Allow or deny creating the object.
+
+		.PARAMETER Read
+			Allow or deny permission to view the object's properties.
+
+		.PARAMETER Edit
+			Allow or deny permission to modify the object.
+
+		.PARAMETER Delete
+			Allow or deny permission to delete the object.
+
+		.PARAMETER DeleteRevisionFromRecycleBin
+			Allow or deny permission to delete from the Recycle Bin.
+
+		.PARAMETER DeleteRevision
+			Allow or deny permission to delete a revision.
+
+		.PARAMETER RestoreRevisionFromRecycleBin
+			Allow or deny permission to restore from the Recycle Bin.
+
+		.PARAMETER RestoreRevision
+			Allow or deny permission to restore a previous version of the object.
+
+		.PARAMETER Move
+			Allow or deny permission to move the object from its original folder to another folder.
+
+		.PARAMETER ToggleEnable
+			Allow or deny permission to enable this object if it is currently disabled.
+
+		.PARAMETER ManualRun
+			Allow or deny permission to manually run the object.
+
+		.PARAMETER Stop
+			Allow or deny permission to manually stop the object when it is running.
+
+		.PARAMETER Import
+			Allow or deny permission to import the object.
+
+		.PARAMETER Export
+			Allow or deny permission to export the object.
+
+		.PARAMETER Staging
+			Allow or deny staging for the object.
+
+		.PARAMETER Assign
+			Allow or deny permission to assign the object to a workflow.
+
+		.PARAMETER ChangeSecurity
+			Allow or deny permission to modify the security (permissions) settings of the object.
+
+		.PARAMETER ManualResume
+			Allow or deny the object to resume execution from where it last encountered an error.
+
+		.PARAMETER ManualRunFromHere
+			Allow or deny permission to manually run the object from the specified step or workflow location.
+
+		.PARAMETER ToggleLock
+			Allow or deny permission to lock an object.
+
+		.PARAMETER UpdateRevision
+			Allow or deny permission to update a revision.
+
+		.PARAMETER Upgrade
+			Undocumented permission.
+
         .EXAMPLE
             # Denies user 'John' access to task 'Test'
             Get-AMTask -Name "Test" | New-AMPermission -Principal 'John'
@@ -51,7 +120,6 @@ function New-AMPermission {
         [switch]$Staging = $false,
         [switch]$Assign = $false,
         [switch]$ChangeSecurity = $false,
-        [switch]$CanEdit = $false,
         [switch]$ManualResume = $false,
         [switch]$ManualRunFromHere = $false,
         [switch]$ToggleLock = $false,
@@ -101,7 +169,6 @@ function New-AMPermission {
             $Staging = $true
             $Assign = $true
             $ChangeSecurity = $true
-            $CanEdit = $true
             $ManualResume = $true
             $ManualRunFromHere = $true
             $ToggleLock = $true
