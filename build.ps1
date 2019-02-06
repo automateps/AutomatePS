bd4620fa2db82a50fe46e90a1001ac01b6954e93
David Seibel <david.seibel@fmins.com>
2019-02-06 15:40:52 -0500

    SImplify build script

--- build.ps1
+++ build.ps1
@@ -1,35 +1,10 @@
 using module AutoMatePS  # Expose custom types so PlatyPS can create help
 [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")] param ()
 #requires -Modules PlatyPS
-foreach ($file in Get-ChildItem -Path .\Functions -File -Recurse) {
-    # Don't update the modify dates if the file was just pulled from Github
-    if ($file.CreationTime -ne $file.LastWriteTime) {
-        # Update modify dates
-        $modifyDate = Get-Date $file.LastWriteTime -Format "MM/dd/yyyy"
-        $content = $file | Get-Content
-        $contentChanged = $false
-        :contentloop foreach ($i in 0..($content.Count - 1)) {
-            if ($content[$i] -like "*Date Modified*") {
-                $oldModifyDate = $content[$i].Split(":")[1].Trim()
-                if ($modifyDate -ne $oldModifyDate) {
-                    $content[$i] = $content[$i].Replace($oldModifyDate, $modifyDate)
-                    $contentChanged = $true
-                }
-                break contentloop
-            }
-        }
-        if ($contentChanged) {
-            if ($PSCmdlet.ShouldProcess("Updating help and modify date for $file")) {
-                $content | Set-Content -Path $file.FullName
-                New-MarkdownHelp -Command $file.BaseName -OutputFolder ".\Docs" -Force
-            }
-        }
-    }
-}
 
 # Update module manifest
+$functions = Get-ChildItem ".\Functions\Public\*.ps1" | Select-Object -ExpandProperty BaseName
 if ($PSCmdlet.ShouldProcess("Updating module manifest")) {
-    $functions = Get-ChildItem ".\Functions\Public\*.ps1" | Select-Object -ExpandProperty BaseName
     Update-ModuleManifest -Path ".\AutoMatePS.psd1" -FunctionsToExport $functions
 }
 
@@ -48,7 +23,7 @@ $helps = $functions | ForEach-Object {
 }
 foreach ($help in $helps) {
     # Check for Description
-    if ([string]::IsNullOrEmpty($help.Description)) {
+    if ([string]::IsNullOrEmpty($help.Description.Text)) {
         Write-Warning "$($help.Name) does not have a description defined in help."
     }
     foreach ($parameter in $help.Parameters.Parameter) {
