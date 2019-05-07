4473bba8e399d5719e6daca5aff1d86ac8c56d3e
David Seibel <david.seibel@fmins.com>
2019-05-07 09:09:03 -0400

    New functions Get-AMWorkflowItem and Remove-AMWorkflowItem

---
+++ Functions/Public/Get-AMWorkflowItem.ps1
@@ -0,0 +1,79 @@
+function Get-AMWorkflowItem {
+    <#
+        .SYNOPSIS
+            Gets a list of items within a workflow.
+
+        .DESCRIPTION
+            Get-AMWorkflowItem retrieves links for a workflow.
+
+        .PARAMETER InputObject
+            The object to retrieve links from.
+
+        .PARAMETER LinkType
+            Only retrieve variables of a specific link type.
+
+        .INPUTS
+            The following AutoMate object types can be queried by this function:
+            Workflow
+
+        .EXAMPLE
+            # Get links in workflow "FTP Files"
+            Get-AMWorkflow "FTP Files" | Get-AMWorkflowLink
+
+        .LINK
+            https://github.com/AutomatePS/AutomatePS
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        $InputObject,
+
+        [ValidateNotNullOrEmpty()]
+        [AMLinkType]$LinkType
+    )
+
+    BEGIN {
+        $constructCache = @{}
+        $agentCache = @{}
+    }
+
+    PROCESS {
+        foreach ($obj in $InputObject) {
+            Write-Verbose "Processing $($obj.Type) '$($obj.Name)'"
+            if (-not $constructCache.ContainsKey($obj.ConnectionAlias)) {
+                $constructCache.Add($obj.ConnectionAlias, @())
+            }
+            if (-not $agentCache.ContainsKey($obj.ConnectionAlias)) {
+                $agentCache.Add($obj.ConnectionAlias, @())
+            }
+            if ($obj.Type -eq "Workflow") {
+                $allItems = @($obj.Triggers) + @($obj.Items)
+                foreach ($item in $allItems) {
+                    if (($item | Get-Member -Name Construct | Measure-Object).Count -eq 0) {
+                        if (-not [string]::IsNullOrEmpty($item.ConstructID)) {
+                            if ($constructCache[$obj.ConnectionAlias].ID -notcontains $item.ConstructID) {
+                                $constructCache[$obj.ConnectionAlias] += Get-AMObject -ID $item.ConstructID -Types $item.ConstructType -Connection $obj.ConnectionAlias
+                            }
+                            $construct = $constructCache[$obj.ConnectionAlias] | Where-Object {$_.ID -eq $item.ConstructID}
+                        }
+                        $item | Add-Member -MemberType NoteProperty -Name Construct -Value $construct
+                    }
+                    if (($item | Get-Member -Name Agent | Measure-Object).Count -eq 0) {
+                        if (-not [string]::IsNullOrEmpty($item.AgentID)) {
+                            if ($agentCache[$obj.ConnectionAlias].ID -notcontains $item.AgentID) {
+                                $agentCache[$obj.ConnectionAlias] += Get-AMObject -ID $item.AgentID -Types "Agent","AgentGroup","SystemAgent" -Connection $obj.ConnectionAlias
+                            }
+                            $agent = $agentCache[$obj.ConnectionAlias] | Where-Object {$_.ID -eq $item.AgentID}
+                        }
+                        $item | Add-Member -MemberType NoteProperty -Name Agent -Value $agent
+                    }
+                    $item.PSObject.TypeNames.Insert(0, "CustomWorkflowItem")
+                    $item
+                }
+            } else {
+                Write-Error -Message "Unsupported input type '$($obj.Type)' encountered!" -TargetObject $obj
+            }
+        }
+    }
+}
---
+++ Functions/Public/Remove-AMWorkflowItem.ps1
@@ -0,0 +1,81 @@
+function Remove-AMWorkflowItem {
+    <#
+        .SYNOPSIS
+            Removes an item from an AutoMate Enterprise workflow
+
+        .DESCRIPTION
+            Remove-AMWorkflowItem can remove items from a workflow object.
+
+        .PARAMETER InputObject
+            The item object to remove.
+
+        .PARAMETER ID
+            The ID of the item to remove (if passing in a workflow).
+
+        .INPUTS
+            The following AutoMate object types can be modified by this function:
+            Workflow
+            WorkflowItem
+            WorkflowTrigger
+
+        .OUTPUTS
+            None
+
+        .EXAMPLE
+            # Remove all triggers from workflow "Some Workflow"
+            (Get-AMWorkflow "Some Workflow").Triggers | Remove-AMWorkflowItem
+
+        .LINK
+            https://github.com/AutomatePS/AutomatePS
+    #>
+    [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact="Medium")]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        $InputObject,
+
+        [Parameter(Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ID
+    )
+
+    PROCESS {
+        :objectloop foreach ($obj in $InputObject) {
+            $shouldUpdate = $false
+            switch ($obj.Type) {
+                "Workflow" {
+                    $updateObject = Get-AMWorkflow -ID $obj.ID -Connection $obj.ConnectionAlias
+                    if (($updateObject | Measure-Object).Count -eq 1) {
+                        $updateObject.Items = @($updateObject.Items | Where-Object {$_.ID -ne $ID})
+                        $updateObject.Triggers = @($updateObject.Triggers | Where-Object {$_.ID -ne $ID})
+                        $updateObject.Links = @($updateObject.Links | Where-Object {$_.SourceID -ne $ID -and $_.DestinationID -ne $ID})
+                        $shouldUpdate = $true
+                    } else {
+                        Write-Warning "Multiple workflows found for ID $($obj.ID)! No action will be taken."
+                        continue objectloop
+                    }
+                }
+                {$_ -in "WorkflowCondition","WorkflowItem","WorkflowTrigger"} {
+                    $updateObject = Get-AMObject -ID $obj.WorkflowID -Types Workflow
+                    if (($updateObject | Measure-Object).Count -eq 1) {
+                        $updateObject.Items = @($updateObject.Items | Where-Object {$_.ID -ne $obj.ID})
+                        $updateObject.Triggers = @($updateObject.Triggers | Where-Object {$_.ID -ne $obj.ID})
+                        $updateObject.Links = @($updateObject.Links | Where-Object {$_.SourceID -ne $obj.ID -and $_.DestinationID -ne $obj.ID})
+                        $shouldUpdate = $true
+                    } else {
+                        Write-Warning "Multiple workflows found for ID $($obj.WorkflowID)! No action will be taken."
+                        continue objectloop
+                    }
+                }
+                default {
+                    Write-Error -Message "Unsupported input type '$($obj.Type)' encountered!" -TargetObject $obj
+                }
+            }
+            if ($shouldUpdate) {
+                $updateObject | Set-AMObject
+            } else {
+                Write-Verbose "$($updateObject.Type) '$($updateObject.Name)' does not contain a link with ID $($obj.ID)."
+            }
+        }
+    }
+}
