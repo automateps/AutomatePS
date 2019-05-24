8a58a6d3da271dc0cf311b8087eadf2e826ebe5b
David Seibel <david.seibel@fmins.com>
2019-05-24 09:10:43 -0400

    Add warning for unsupported PowerShell Core

--- AutoMatePS.psm1
+++ AutoMatePS.psm1
@@ -10,3 +10,7 @@ Get-ChildItem -Path "$PSScriptRoot\Functions\Private\*.ps1" | ForEach-Object {
 
 Set-Variable -Name AMScheduleDateFormat -Value "MM/dd/yyyy HH:mm:ss" -Scope Global
 Set-Variable -Name AMGuidRegex -Value "^(\{{0,1}([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}\}{0,1})$" -Scope Global
+
+if ($global:PSVersionTable.PSVersion.Major -gt 5) {
+    Write-Warning "AutomatePS is not fully compatible with PowerShell Core (v6 and greater) due to a JSON conversion issue (see https://github.com/PowerShell/PowerShell/issues/9610 for specifics).  'Get' functions will work.  However, most functions that modify objects will not work, or may produce unexpected results.  Use at your own risk."
+}
